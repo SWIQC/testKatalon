d2f19f66849324e1ea72892b165792c6efd1cc3b
calba13 <wiwied.qc1@gmail.com>
2024-06-03 17:10:28 +0700

    add TS

---
+++ Test Suites/TS1. Open Browser/Open Browser.ts
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Open Browser</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>c484eaa4-2474-41bd-9e2e-e603737f60e7</testSuiteGuid>
+   <testCaseLink>
+      <guid>33339c3e-ce4f-434d-851a-9b4c94fa8852</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/00 Common/Open Browser with set Address (URL)</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>56932eeb-2a9c-46ff-aa81-c46b23b7b6e6</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>4bb67b35-5fff-47f5-b87a-1b1d3db5e17d</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
