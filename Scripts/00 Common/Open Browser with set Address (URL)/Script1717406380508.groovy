12294d31d83f7f9582a4dd349d8131bad32a7152
calba13 <wiwied.qc1@gmail.com>
2024-07-24 13:36:08 +0700

    result coding automation test

--- Scripts/00 Common/Open Browser with set Address (URL)/Script1717406380508.groovy
+++ Scripts/00 Common/Open Browser with set Address (URL)/Script1717406380508.groovy
@@ -1,12 +1,90 @@
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
 
-import internal.GlobalVariable
+// Open Browser and navigate to BASEURL
+WebUI.openBrowser(GlobalVariable.BASEURL);
+WebUI.maximizeWindow();
+WebUI.delay(3);
 
-'OPEN BROWSER + SET URL'
-WebUI.openBrowser(baseurl)
+// Click on Eagle Score Menu
+WebUI.click(findTestObject('Object Repository/Page_LIPS.OTO/eagleScoreMenu'));
 
-'MAX WIN'
-WebUI.maximizeWindow()
+// Set text fields
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_LIPS.OTO/companyNameField'), companyName);
+WebUI.delay(2);
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_LIPS.OTO/productTypeField'), productType);
+WebUI.delay(3);
+WebUI.sendKeys(findTestObject('Object Repository/Page_LIPS.OTO/ktpField'), ktp);
+WebUI.sendKeys(findTestObject('Object Repository/Page_LIPS.OTO/nameField'), name);
+WebUI.sendKeys(findTestObject('Object Repository/Page_LIPS.OTO/birthDateField'), birthDate);
 
-'LOGIN PROFIL with ActiveDirectory'
-not_run: CustomKeywords.'customKW.LoginSecurityWebBrowser.loginAutoITProfile'(GlobalVariable.SCREEN_MAXSIZE, loginProfile)
+// Click Submit Button
+WebUI.click(findTestObject('Object Repository/Page_LIPS.OTO/submitBtn'));
+
+
+//=======================DEBUGGING=========================
+
+//// Number of retries allowed
+//int maxRetries = 3;
+//int retryCount = 0;
+//boolean retry = true;
+//
+//while (retry && retryCount < maxRetries) {
+//    try {
+//        // Open Browser and navigate to BASEURL
+//        WebUI.openBrowser(GlobalVariable.BASEURL);
+//        WebUI.maximizeWindow();
+//        WebUI.delay(3);
+//
+//        // Click on Eagle Score Menu
+//        WebUI.click(findTestObject('Object Repository/Page_LIPS.OTO/eagleScoreMenu'));
+//
+//        // Set text fields
+//        WebUI.setText(findTestObject('Object Repository/Page_LIPS.OTO/companyNameField'), companyName);
+//        WebUI.setText(findTestObject('Object Repository/Page_LIPS.OTO/productTypeField'), productType);
+//        WebUI.setText(findTestObject('Object Repository/Page_LIPS.OTO/ktpField'), ktp);
+//        WebUI.setText(findTestObject('Object Repository/Page_LIPS.OTO/nameField'), name);
+//        WebUI.setText(findTestObject('Object Repository/Page_LIPS.OTO/birthDateField'), birthDate);
+//
+//        // Click Submit Button
+//        WebUI.click(findTestObject('Object Repository/Page_LIPS.OTO/submitBtn'));
+//	
+//		// failed condition
+//        if (WebUI.verifyTextPresent("Criteria not match", false, FailureHandling.OPTIONAL)) {
+//            WebUI.takeScreenshot();
+//        } else if (WebUI.verifyTextPresent("Customer Info not Available", false, FailureHandling.OPTIONAL)) {
+//            WebUI.takeScreenshot();
+//        } else {
+//            //success
+//            WebUI.takeScreenshot();
+//        }
+//
+//        // If there is no error , exit retry loop
+//        retry = false;
+//
+//    } catch (StepFailedException e) {
+//        retryCount++;
+//        if (retryCount >= maxRetries) {
+//            WebUI.takeScreenshot();
+//        } else {
+//            WebUI.comment("Attempt " + retryCount + " failed. Retrying...");
+//        }
+//    } finally {
+//        // Close browser after each attempt (optional)
+//        WebUI.closeBrowser();
+//    }
+//}
--- Test Suites/TS1. Open Browser/Open Browser.ts
+++ Test Suites/TS1. Open Browser/Open Browser.ts
@@ -5,7 +5,7 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>3</numberOfRerun>
+   <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
@@ -16,6 +16,15 @@
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/00 Common/Open Browser with set Address (URL)</testCaseId>
+      <testDataLink>
+         <combinationType>MANY</combinationType>
+         <id>5d09cdc8-fc77-4d15-9ab1-2ad5a8bedeee</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value>1-5</value>
+         </iterationEntity>
+         <testDataId>Data Files/SampleDataInquiry</testDataId>
+      </testDataLink>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
       <variableLink>
          <testDataLinkId></testDataLinkId>
@@ -28,6 +37,36 @@
          <type>DEFAULT</type>
          <value></value>
          <variableId>4bb67b35-5fff-47f5-b87a-1b1d3db5e17d</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>5d09cdc8-fc77-4d15-9ab1-2ad5a8bedeee</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Company</value>
+         <variableId>e707ff48-3597-4d30-83b8-60fed5500654</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>5d09cdc8-fc77-4d15-9ab1-2ad5a8bedeee</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>productType</value>
+         <variableId>554d11e6-8db2-43a2-86be-2f2f4e03f848</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>5d09cdc8-fc77-4d15-9ab1-2ad5a8bedeee</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>KTP</value>
+         <variableId>40b228bb-7244-43de-b297-89752a2c96b4</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>5d09cdc8-fc77-4d15-9ab1-2ad5a8bedeee</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Name</value>
+         <variableId>75821d0e-efdf-429f-803a-1825d0b9be8e</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>5d09cdc8-fc77-4d15-9ab1-2ad5a8bedeee</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>DOB</value>
+         <variableId>5b01de2b-2d3e-4f04-8669-f2d044cd813f</variableId>
       </variableLink>
    </testCaseLink>
 </TestSuiteEntity>
